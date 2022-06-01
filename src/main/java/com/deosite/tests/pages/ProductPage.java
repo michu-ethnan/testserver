3b771004539410a73f5f3f29d1d7a4fcf46b8dbe
Abi <tekalign.massebo@deosite.com>
2022-06-01 15:05:36 +0200

    commit 10

--- src/main/java/com/deosite/tests/pages/ProductPage.java
+++ src/main/java/com/deosite/tests/pages/ProductPage.java
@@ -8,10 +8,18 @@ public class ProductPage {
     public static Target CONFIGURABLE_SELECT = Target.the("Configurable select").locatedBy("//*[@id=\"__app__\"]/div/div[3]/div/div[1]/div/div/div/div[3]/div/div[3]/div[1]/div/div/label/select");
     public static Target PRODUCT_NAME = Target.the("Product name").locatedBy("//*[@id=\"__app__\"]//h1");
     public static Target CONFIGURABLE_SELECT_LABEL = Target.the("Configurable select name").locatedBy("//*[@id=\"__app__\"]/div/div[3]/div/div[1]/div/div/div/div[3]/div/div[3]/div[1]/div/div/label/span");
-    public static Target PRODUCT_PRICE = Target.the("Product price").locatedBy("(//span[@data-test='total-checkout'])[3]");
+
+    public static Target PRODUCT_PRICE = Target.the("Product price").locatedBy("//*[@id=\"__app__\"]/div[2]//div[2]/p/span/span[1]");
+    public static Target DISCOUNTED_PRODUCT_PRICE = Target.the("Discounted Product price").locatedBy("//*[@id=\"__app__\"]/div[2]//div[3]/p[2]/span/span[1]");
+
     public static Target RIGHT_ARROW = Target.the("Right arrow").locatedBy("//button[@data-test='right-arrow-small-image-slider']");
     public static Target LEFT_ARROW = Target.the("Left arrow").locatedBy("//button[@data-test='left-arrow-small-image-slider']");
-    public static Target OTHER_PRODUCTS_HEADING = Target.the("Other products heading").locatedBy("(//*[@id=\"__app__\"]//h3)[1]");
     public static Target PRODUCT_THUMBNAIL = Target.the("Product thumbnail").locatedBy("//*[@id=\"__app__\"]/div[2]/div[3]/div/div[2]/div/div/div/div[1]/div[2]/div[1]/div/div[3]");
+
+    public static Target OTHER_PRODUCTS_HEADING = Target.the("Other products heading").locatedBy("(//*[@id=\"__app__\"]//h3)[1]");
+    public static Target ADD_TO_CART_FROM_COLLECTION_PAGE = Target.the("Subcategory list").locatedBy("(//div[@class=\"css-6l1kfd\"]//div[@class=\"css-1n8bjx5\"])[1]");
+
+    public static Target PAGE_NOT_FOUND = Target.the("Page not found").locatedBy("//*[@id=\"__app__\"]/div[2]/div[3]/div/div[2]/div/div/p");
+
 
 }
--- src/main/java/com/deosite/tests/questions/product/ProductPrice.java
+++ src/main/java/com/deosite/tests/questions/product/ProductPrice.java
@@ -1,23 +1,29 @@
 package com.deosite.tests.questions.product;
 
-import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.annotations.Subject;
 
-import java.math.BigDecimal;
+import java.math.*;
 
+import static com.deosite.tests.pages.ProductPage.DISCOUNTED_PRODUCT_PRICE;
 import static com.deosite.tests.pages.ProductPage.PRODUCT_PRICE;
 
 @Subject("Product price")
 
 public class ProductPrice implements Question<BigDecimal> {
-
+    String ProductPrice;
     @Override
     public BigDecimal answeredBy(Actor actor) {
-       String productPrice = PRODUCT_PRICE.resolveFor(actor).getText();
-       String priceAfterTrim = productPrice.replace(",", ".").trim();
-       BigDecimal price = new BigDecimal(priceAfterTrim);
-       return price;
+        if(DISCOUNTED_PRODUCT_PRICE.resolveFor(actor).isPresent()){
+            ProductPrice = DISCOUNTED_PRODUCT_PRICE.resolveFor(actor).getText();
+        }else {
+            ProductPrice = PRODUCT_PRICE.resolveFor(actor).getText();
+        }
+
+        String priceAfterTrim = ProductPrice.replace(",", ".").trim();
+        BigDecimal price = new BigDecimal(priceAfterTrim);
+        return price;
     }
 
     public static Question<BigDecimal> price() {
